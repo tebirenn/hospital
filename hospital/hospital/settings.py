d5777c2c732a79e61559d75ec81ad6ddc1f59012
unknown <a_saudabekov@kbtu.kz>
2023-11-25 11:06:43 +0300

    connected to postgres

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -56,8 +56,12 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
